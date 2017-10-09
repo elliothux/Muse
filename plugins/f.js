b8d4b494264253c888a8d177ef9f58ef0ee23da3
HuQingyang <i@do.codes>
2017-10-09 19:47:57 +0800

    Building babel-plugin to compile JSX...

--- plugins/f.js
+++ plugins/f.js
@@ -1,17 +1,35 @@
 
 const colors = require('colors/safe');
+const log = function (text) {
+    typeof text === 'object' && (text = JSON.stringify(text, null, 4));
+    console.log(colors.red.bold(text))
+}.bind(console);
+
 
 module.exports = function (babel) {
+    const getAndRemoveBindingAttr = (jsxElement, attrName) => {
+        const { attributes } = jsxElement.openingElement;
+        let value;
+        const index = attributes.findIndex((attr, index) => {
+            if (attr.name && attr.name.name === attrName) {
+                value = attr.value;
+                return true
+            }
+        });
+        if (index !== -1) {
+            attributes.splice(index, 1)
+        }
+        return value
+    };
+
     return {
         visitor: {
-            // JSXIdentifier: function(path) {
-            //     console.log(colors.red.underline("Visiting: " + path.node.name));
-            // },
-            // JSXAttribute: function(path) {
-            //     console.log(colors.blue.underline("Visiting: " + path.node.name));
-            // },
             JSXElement: function(path, state) {
-                console.log(colors.red.bold.underline("Visiting: " + path));
+                const { attributes } = path.node.openingElement;
+                log(attributes)
+                // log(path.node.openingElement);
+                // log(this.opts)
+                // const { attrName } = this.opts;
             }
         }
     }
--- test/index.js
+++ test/index.js
@@ -3,39 +3,25 @@ import { F } from '../lib';
 
 
 
-class App extends F {
+
+
+class App2 extends F {
     constructor() {
         super();
-        this.state = {
-            name: 'Joe',
-            age: 10
-        };
-        this.handleClick = this.handleClick.bind(this);
-        this.handleName = this.handleName.bind(this);
+        this.state ={
+            age: 20
+        }
     }
 
-    handleName(e) {
-        this.setState({
-            name: e.target.value
-        })
-    }
-
-    handleClick() {
-        this.setState({
-            age: this.state.age + 1
-        })
-    }
-
-    render() { return (
-        <div>
-            <h3>My name is {this.state.name}</h3>
-            <h4>And I'm {this.state.age} years old</h4>
-            <input type="text" value={this.state.name} onInput={this.handleName}/>
-            <button onClick={this.handleClick}>Click</button>
-        </div>
+    render() { return(
+        <input
+            type="number"
+            model={this.state.age}
+            placeholder="Hello"
+        />
     )}
 }
 
 
-const app = (new App()).renderTo(document.getElementById('root'));
+const app = (new App2()).renderTo(document.getElementById('root'));
 console.log(app);
